2173399054e91a54db3b818e5a2740e38671b93c
Dan Lopuch <dan@datahero.com>
2016-07-28 14:01:19 -0700

    Creating first test

--- public/js/entry.js
+++ public/js/entry.js
@@ -3,5 +3,5 @@ const content = require('./content.js');
 window.onload = function onload() {
   document.getElementById('content_div').innerHTML = `yo dan! ${content}`;
 
-  console.log('not so fast, FIXME: linting violation no semicolon');
+  console.log('not so fast, FIXME: linting violation no semicolon')
 };
